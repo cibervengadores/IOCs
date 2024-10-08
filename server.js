9f7c346d89bd87d232bf71321db529acafbe504f
CuriosidadesDeHackers <curiosidadesdehackers@CuriosidadesDeHackers>
2024-10-08 19:03:20 +0200

    email

--- server.js
+++ server.js
@@ -6,30 +6,34 @@ require('dotenv').config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const git = simpleGit();
 
-const GITHUB_REPO = process.env.GITHUB_REPO; // Asegúrate de que esto sea correcto
+const GITHUB_REPO = process.env.GITHUB_REPO;
 const GITHUB_USER = process.env.GITHUB_USER;
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-const FILE_PATH = 'peticiones.md'; // Cambiado a peticiones.md
+const FILE_PATH = 'peticiones.md';
 
 // Función para añadir la petición al archivo peticiones.md
 const addToFile = async (petition) => {
   try {
-    // Asegúrate de que el archivo existe y si no, lo crea
+    // Asegúrate de que el archivo existe
     if (!fs.existsSync(FILE_PATH)) {
-      fs.writeFileSync(FILE_PATH, ''); // Crea el archivo si no existe
+      fs.writeFileSync(FILE_PATH, ''); // Crear archivo si no existe
     }
 
-    // Agregar la petición al archivo
+    // Añadir la petición al archivo
     fs.appendFileSync(FILE_PATH, `${petition}\n`);
     console.log('Petición añadida:', petition);
 
+    // Configurar identidad de usuario de Git
+    await git.addConfig('user.email', 'cibervengadores@proton.me');
+    await git.addConfig('user.name', 'cibervengadores');
+
     // Guardar los cambios en GitHub
     const gitUrl = `https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPO}.git`;
-    
+
     // Añadir, commitear y hacer push a GitHub
     await git.add(FILE_PATH);
     await git.commit(`Add petition: ${petition}`);
-    await git.push(gitUrl, 'main'); // Cambia 'main' por tu rama principal si es necesario
+    await git.push(gitUrl, 'main'); // Cambia 'main' por tu rama si es necesario
     console.log('Cambios enviados a GitHub');
   } catch (error) {
     console.error('Error guardando en GitHub:', error);
@@ -54,15 +58,15 @@ bot.launch().then(() => {
   console.error('Error al lanzar el bot:', error);
 });
 
-// Código para manejar el webhook
+// Webhook
 const express = require('express');
 const app = express();
 
-app.use(bot.webhookCallback('/bot')); // Establece el webhook
+app.use(bot.webhookCallback('/bot'));
 
-const PORT = process.env.PORT || 3000; // Usa el puerto configurado en la variable de entorno o el 3000 por defecto
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
 
-module.exports = app; // Exporta la app para usar con un servidor Express si es necesario
+module.exports = app;
